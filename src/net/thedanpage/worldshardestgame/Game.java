36ea4c7e7393da92e95a49663654beba746e2bc0
Frederik Dam Christensen <fdc@shape.dk>
2018-11-02 12:14:29 +0100

    make game abstract

--- src/net/thedanpage/worldshardestgame/Game.java
+++ src/net/thedanpage/worldshardestgame/Game.java
@@ -19,9 +19,8 @@ import static net.thedanpage.worldshardestgame.Sound.COIN;
 public abstract class Game extends JPanel implements ActionListener {
 
     Timer t = new Timer(5, this);
-    List<Player> population = new ArrayList<>();
+    List<Player> population;
 
-    int generation = 1;
     boolean goalReached = false;
     boolean running = false;
 
@@ -32,17 +31,15 @@ public abstract class Game extends JPanel implements ActionListener {
     public Game(Controller controller, GameLevel level) {
         this.controller = controller;
         this.level = level;
-        intializePopulation();
+        population = initializePopulation();
     }
 
-    public boolean goalReached() { return goalReached; }
-    public void goalReached(boolean goalReached) { this.goalReached = goalReached; }
+    public abstract int generationCount();
+    public abstract void populationIsDead();
+    public abstract void playerIsDead(Player player);
+    public abstract List<Player> initializePopulation();
 
     public GameLevel getLevel() { return level; }
-
-    public double calculateFitness(Player player) {
-        return getLevel().getDistanceToGoal(player);
-    }
 
     public void paintComponent(final Graphics g) {
         super.paintComponent(g);
@@ -78,7 +75,8 @@ public abstract class Game extends JPanel implements ActionListener {
         g.fillRect(0, 0, 800, 22);
         g.setColor(Color.WHITE);
         g.setFont(new Font("Tahoma", Font.BOLD, 18));
-        drawRightJustifiedString("Generation: " + this.generation, 750, 17, g);
+        var count = generationCount();
+        drawRightJustifiedString(count > 0 ? "Generation: " + count : "", 750, 17, g);
         g.dispose();
     }
 
@@ -92,11 +90,11 @@ public abstract class Game extends JPanel implements ActionListener {
             if(player.isDead()) deadPlayerCount++;
         }
 
-        if(deadPlayerCount == populationSize) {
-            evaluateGeneration();
+        if(deadPlayerCount == population.size()) {
+            populationIsDead();
         }
 
-        if (goalReached()) {
+        if (goalReached) {
             System.out.println("Goal Reached!");
             System.exit(0);
         }
@@ -122,7 +120,7 @@ public abstract class Game extends JPanel implements ActionListener {
             if (level.allCoinsCollected()) {
                 for (Tile t : level.getTileMap()) {
                     if (t.getType() == 3 && player.collidesWith(t.getBounds())) {
-                        goalReached(true);
+                        goalReached = true;
                         winningPlayer = player;
                     }
                 }
@@ -147,7 +145,7 @@ public abstract class Game extends JPanel implements ActionListener {
             }
         }
         if (player.isDead()) {
-            player.fitness = calculateFitness(player);
+            playerIsDead(player);
             player.opacity = 0;
         }
     }
@@ -161,50 +159,6 @@ public abstract class Game extends JPanel implements ActionListener {
         checkIfDead(level, player);
     }
 
-    public void evaluateGeneration() {
-        this.generation++;
-        if(this.generation % 5 == 0) {
-            if (this.playerMoveCount < 5000) this.playerMoveCount *= 1.2;
-        }
-
-        var bestCandidates = selection();
-        var newPopulation = mutate(bestCandidates);
-        population = newPopulation;
-        getLevel().reset();
-        for(Player player : population) {
-            player.respawn(getLevel());
-        }
-
-        System.out.println("Fitness: " + bestCandidates.get(0).fitness + " MoveCount: " + bestCandidates.get(0).getMoves().size());
-    }
-
-    public List<Player> selection() {
-        Player bestPlayer = null;
-        double bestFitness = Double.MAX_VALUE;
-        for(var player : population) {
-            if(player.fitness < bestFitness) {
-                bestPlayer = player;
-                bestFitness = player.fitness;
-            }
-        }
-        return Arrays.asList(bestPlayer);
-    }
-
-    public List<Player> mutate(List<Player> candidates) {
-        List<Player> children = new ArrayList<>();
-        var childrenCountPerCandidate = populationSize / candidates.size();
-        for(var candidate : candidates) {
-            for(var i = 0; i < childrenCountPerCandidate; i++) {
-                var child = new Player(this.playerMoveCount, candidate.getMoves());
-                child.mutate();
-                children.add(child);
-            }
-        }
-        return children;
-    }
-
-
-
     public void actionPerformed(ActionEvent arg0) {
         repaint();
     }
@@ -213,13 +167,5 @@ public abstract class Game extends JPanel implements ActionListener {
         FontMetrics fm = g.getFontMetrics();
         int x = (w - fm.stringWidth(s));
         g.drawString(s, x, h);
-    }
-
-    private void intializePopulation() {
-        for (var i = 0; i < populationSize; i++) {
-            var player = new Player(playerMoveCount);
-            player.respawn(getLevel());
-            population.add(player);
-        }
     }
 }
